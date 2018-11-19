f44fe93a6541b2f2c30acc2f3b637c1eb7441d3e
Christoph Engelbert <me@noctarius.com>
2018-11-19 09:50:22 +0100

    simplified app.js

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ RUN mkdir /root/app
 COPY package.json /root/app/package.json
 COPY app.js /root/app/app.js
 ADD bin /root/app/bin
-ADD public /root/app/public
-ADD routes /root/app/routes
 
 WORKDIR /root/app
 RUN apk add --no-cache --virtual .gyp build-base python && npm install --only=production && apk del .gyp
--- app.js
+++ app.js
@@ -4,15 +4,10 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+app.get('/', function(req, res, next) {
+  res.send('nowait');
+});
 
 module.exports = app;
